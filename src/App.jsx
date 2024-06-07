4dd4aa0321f0edf979d4241ac4bb67bbb5a63a4f
Divyansh <ssdivyansh9140@gmail.com>
2024-06-07 20:40:43 +0530

    kerberos integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Dummy from './pages/dummy/dummy'
+import Login from './pages/login/login'
 
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Home />} />
       <Route path="/dummy" elemnt={<Dummy />} />
     </Routes>
     </BrowserRouter>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,27 @@
+import React, { useState } from 'react';
 import Dashboard from "../../components/Dashboard";
 import List from "../../components/List";
 import Sidebar from "../../components/Sidebar";
 
 export default function Home() {
+    const [isSidebarVisible, setSidebarVisible] = useState(true);
+
     return (
         <div className="grid grid-cols-10">
-            <div className="col-span-2 ">
-            <Sidebar />
-            </div>
-            <div className="col-span-8">
-            <Dashboard />
-            <div className="shadow-lg p-4 rounded-sm">
-                <div className="text-2xl text-sky-500 font-bold">Welcome User!</div>
-                <List />
-            </div>
+            {isSidebarVisible && (
+                <div className="col-span-2">
+                    <Sidebar onClose={() => setSidebarVisible(false)} />
+                </div>
+            )}
+            <div className={`col-span-${isSidebarVisible ? '8' : '10'}`}>
+                <button onClick={() => setSidebarVisible(!isSidebarVisible)}>
+                    Toggle Sidebar
+                </button>
+                <Dashboard />
+                <div className="shadow-lg p-4 rounded-sm">
+                    <div className="text-2xl text-sky-500 font-bold">Welcome User!</div>
+                    <List />
+                </div>
             </div>
         </div>
     )
