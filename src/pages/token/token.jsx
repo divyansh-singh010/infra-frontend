9af41267800022bab1e7430719f55d1a769dc1af
Divyansh <ssdivyansh9140@gmail.com>
2024-06-11 01:01:22 +0530

    minor change

--- src/pages/token/token.jsx
+++ src/pages/token/token.jsx
@@ -14,7 +14,7 @@ const Token = () => {
         })
             .then(res => {
                 localStorage.setItem('user', JSON.stringify(res.data.profile));
-                localStorage.setItem('access_token', token);
+                localStorage.setItem('token', token);
                 setVerified(true);
                 window.location.href = '/dashboard';
             })
