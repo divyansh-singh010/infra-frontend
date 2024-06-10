f1947eb62fef2df8b19ad7194b63f9cbcc4448ed
Divyansh <ssdivyansh9140@gmail.com>
2024-06-10 19:51:08 +0530

    unauthorised error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ import Issue from './pages/home/Issue'
 import Room from './pages/home/Room'
 import Login from './pages/login/Login'
 import { useEffect, useState } from "react";
-import Token from './pages/token/token'
-import Login from './pages/login/login'
+import Token from './pages/token/token';
 
 function App() {
   const [isOpen, setIsOpen] = useState(true)
@@ -18,23 +17,26 @@ function App() {
     setIsOpen(!isOpen);
   }
   const [queries, setQueries] = useState([])
-    useEffect(() => {
-        axios.get(`${DOMAIN}queries/`)
-            .then((response) => {
-                console.log(response.data);
-                setQueries(response.data.queries);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+  useEffect(() => {
+    axios.get(`${DOMAIN}queries/`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+        setQueries(response.data.queries);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    // Create frequency mapping for 'room'
-const roomFrequency = queries.reduce((acc, query) => {
+  const roomFrequency = queries.reduce((acc, query) => {
     acc[query.room] = (acc[query.room] || 0) + 1;
     return acc;
   }, {});
-  
+
   // Convert the frequency mapping to the desired format
   const sortedRoomFrequency = Object.entries(roomFrequency).sort((a, b) => b[1] - a[1]);
 
@@ -42,7 +44,7 @@ const roomFrequency = queries.reduce((acc, query) => {
     acc[query.issue] = (acc[query.issue] || 0) + 1;
     return acc;
   }, {});
-  
+
   // Convert the frequency mapping to the desired format
   const issueFrequencyList = [["Issue", "Frequency"], ...Object.entries(issueFrequency).map(([issue, freq]) => [issue, freq])];
   const issue_options = {
@@ -61,7 +63,7 @@ const roomFrequency = queries.reduce((acc, query) => {
     acc[query.complex_name] = (acc[query.complex_name] || 0) + 1;
     return acc;
   }, {});
-  
+
   // Convert the frequency mapping to the desired format
   const complexFrequencyList = [["Complex", "Frequency"], ...Object.entries(complexFrequency).map(([complex_name, freq]) => [complex_name, freq])];
   const complex_options = {
@@ -71,15 +73,15 @@ const roomFrequency = queries.reduce((acc, query) => {
   return (
     <BrowserRouter>
       <Routes>
-      {/* <Route path="/" element={<Login />} /> */}
+        {/* <Route path="/" element={<Login />} /> */}
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Home isOpen={isOpen} toggleSidebar={toggleSidebar} queries={queries} complexFrequencyList={complexFrequencyList} complex_options={complex_options} issueFrequencyList={issueFrequencyList} issue_options={issue_options} roomFrequencyList={sortedRoomFrequency} />} />
         <Route path="/token" element={<Token />} />
-        <Route path="/dummy" elemnt={<Dummy isOpen={isOpen} toggleSidebar={toggleSidebar}/>} />
-      <Route path="/complex" element={<Complex isOpen={isOpen} toggleSidebar={toggleSidebar} complexFrequencyList={complexFrequencyList} complex_options={complex_options} />} />
-       <Route path="/queries" element={<Queries isOpen={isOpen} toggleSidebar={toggleSidebar} queries={queries}/>} />
-      <Route path="/issue" element={<Issue isOpen={isOpen} toggleSidebar={toggleSidebar} issueFrequencyList={issueFrequencyList} issue_options={issue_options}/>} />
-      <Route path="/room" element={<Room isOpen={isOpen} toggleSidebar={toggleSidebar} roomFrequencyList={sortedRoomFrequency}/>} />
+        <Route path="/dummy" elemnt={<Dummy isOpen={isOpen} toggleSidebar={toggleSidebar} />} />
+        <Route path="/complex" element={<Complex isOpen={isOpen} toggleSidebar={toggleSidebar} complexFrequencyList={complexFrequencyList} complex_options={complex_options} />} />
+        <Route path="/queries" element={<Queries isOpen={isOpen} toggleSidebar={toggleSidebar} queries={queries} />} />
+        <Route path="/issue" element={<Issue isOpen={isOpen} toggleSidebar={toggleSidebar} issueFrequencyList={issueFrequencyList} issue_options={issue_options} />} />
+        <Route path="/room" element={<Room isOpen={isOpen} toggleSidebar={toggleSidebar} roomFrequencyList={sortedRoomFrequency} />} />
       </Routes>
     </BrowserRouter>
   )
